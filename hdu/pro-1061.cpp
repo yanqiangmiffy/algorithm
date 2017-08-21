766b8fb8e41683605bb64388fe5d5b39431b6cbc
quincyqiang <yanqiangmiffy@gmail.com>
2017-08-21 14:04:01 +0800

    shulun

--- hdu/pro-1061.cpp
+++ hdu/pro-1061.cpp
@@ -11,53 +11,67 @@ int main(){
         }else if(last==4){
             if(n%2==1){
                 cout<<4<<endl;
-            }else{
+            }
+            if(n%2==0){
                 cout<<6<<endl;
             }
         }else if(last==9){
             if(n%2==1){
-                cout<<1<<endl;
-            }else{
                 cout<<9<<endl;
+            }
+            if(n%2==0){
+                cout<<1<<endl;
             }
         }else if(last==2){
             if(n%4==1){
                 cout<<2<<endl;
-            }else if(n%4==2){
+            }
+            if(n%4==2){
                 cout<<4<<endl;
-            }else if(n%4==3){
+            }
+            if(n%4==3){
                 cout<<8<<endl;
-            }else{
+            }
+            if(n%4==0){
                 cout<<6<<endl;
             }
         }else if(last==3){
             if(n%4==1){
                 cout<<3<<endl;
-            }else if(n%4==2){
+            }
+            if(n%4==2){
                 cout<<9<<endl;
-            }else if(n%4==3){
+            }
+            if(n%4==3){
                 cout<<7<<endl;
-            }else{
+            }
+            if(n%4==0){
                 cout<<1<<endl;
             }
         }else if(last==7){
             if(n%4==1){
                 cout<<7<<endl;
-            }else if(n%4==2){
+            }
+            if(n%4==2){
                 cout<<9<<endl;
-            }else if(n%4==3){
+            }
+            if(n%4==3){
                 cout<<3<<endl;
-            }else{
+            }
+            if(n%4==0){
                 cout<<1<<endl;
             }
         }else if(last==8){
             if(n%4==1){
                 cout<<8<<endl;
-            }else if(n%4==2){
+            }
+            if(n%4==2){
                 cout<<4<<endl;
-            }else if(n%4==3){
+            }
+            if(n%4==3){
                 cout<<2<<endl;
-            }else{
+            }
+            if(n%4==0){
                 cout<<6<<endl;
             }
         }
