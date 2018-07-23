07dbc2e8ffaaa0fc2b9d9c9b484585a68b02ac60
yanqaingmiffy <1185918903@qq.com>
2018-07-23 23:24:30 +0800

    7.23

--- others/test.cpp
+++ others/test.cpp
@@ -1,68 +1,30 @@
-#include <cstdio>
-#include <algorithm>
 #include<iostream>
 using namespace std;
-
-#define SIZE 205
-
-struct Data_Type
-{
-	int from, to;
-	bool flag;
-}moving[SIZE];
-
-bool Cmp(const Data_Type a, const Data_Type b)
-{
-	return a.from < b.from;
+int NthPrime(int n){
+        int i = 2, j = 1;
+        while (true) {
+            j = j + 1;
+            if (j > i / j) {
+                n--;
+                if (n == 0)
+                    break;
+                j = 1;
+            }
+            if (i % j == 0) {
+                i++;
+                j = 1;
+            }
+        }
+        return i;
 }
-
-int main(void)
+int main()
 {
-	int i, testNum, n;
-
-	scanf("%d", &testNum);
-	while (testNum-- != 0)
-	{
-		scanf("%d", &n);
-		for (i = 0; i < n; i++)
-		{
-			scanf("%d%d", &moving[i].from, &moving[i].to);
-			if (moving[i].from > moving[i].to)
-			{
-				int temp = moving[i].from;
-				moving[i].from = moving[i].to;
-				moving[i].to = temp;
-			}
-			if (moving[i].from % 2 == 0)
-			{
-				moving[i].from--;
-			}
-			if (moving[i].to % 2 == 1)
-			{
-				moving[i].to++;
-			}
-			moving[i].flag = false;
-		}
-		sort(moving, moving+n, Cmp);
-
-		bool completion = false;
-		int count = -1, priorTo;
-		while (!completion)
-		{
-			completion = true;
-			count++;
-			priorTo = 0;
-			for (i = 0; i < n; i++)
-			{
-				if (!moving[i].flag && moving[i].from > priorTo)
-				{
-					moving[i].flag = true;
-					priorTo = moving[i].to;
-					completion = false;
-				}
-			}
-		}
-		printf("%d\n", count*10);
-	}
-	return 0;
+    int n,num,count=0;     //count代表已经找到了几个素数
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>num;
+        int x=NthPrime(num);
+        cout<<x<<endl;
+    }
+    return 0;
 }
